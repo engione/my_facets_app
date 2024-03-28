66ebc2e68bb25ea35e30e10442c5810d12f38ea0
unknown <kinovalovvasili04@gmail.com>
2024-03-28 18:35:50 +0300

    внёс изменения в questionare

--- src/components/Questionare/Questionare.tsx
+++ src/components/Questionare/Questionare.tsx
@@ -2,25 +2,14 @@ import "./Questionare.scss"
 
 export const Questionare =()=> {
   return (
-    <section className="questionare">
         <div className="form-questionare">
-            <div className="form-questionare_div">
-                <p className="form-questionare_text">Название: Соревнования по футболу</p>
-            </div>
-            <div className="form-questionare_div">
-                <p className="form-questionare_text">Вид спорта: Футбол</p>
-            </div>
-            <div className="form-questionare_div">
-                <p className="form-questionare_text">Место проведения: Текст</p>
-            </div>
-            <div className="form-questionare_div">
-                <p className="form-questionare_text">Дата проведения: TEXT</p>
-            </div>
+            <p className="form-questionare_text">Название: Соревнования по футболу</p>
+            <p className="form-questionare_text">Вид спорта: Футбол</p>
+            <p className="form-questionare_text">Место проведения: Текст</p>
+            <p className="form-questionare_text">Дата проведения: TEXT</p>
             <button className="form-questionare_list_members">
-                <img className="w-[14px] h-[10px]" src="/src/components/Questionare/Vector.png"></img>
                 <p className="form-questionare_list_members_text" >Список участников</p>
             </button>
         </div>
-    </section>
   );
 }
