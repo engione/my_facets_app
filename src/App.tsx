93b5e28e3878f6165ac8f7ef23c0ec6da5aee61f
engione <kudaskinartem98@gmail.com>
2024-03-30 12:03:59 +0300

    account page start

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import "./App.css";
 import { AuthForm } from "./components/AuthForm/Auth";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Questionare } from "./components/Questionare/Questionare";
+import { Aside } from "./components/Aside/Aside";
+import { AccountPage } from "./pages/AccountPage";
 
 
 export function App() {
@@ -12,10 +14,9 @@ export function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="auth" element={<AuthForm />} />
+          <Route path="account" element={<AccountPage />} />
         </Routes>
       </BrowserRouter>
-
-
     </>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,12 +5,13 @@ interface PropsButton {
   children: React.ReactNode;
   className?: string | undefined;
   type?: "submit" | "reset" | "button";
+  bgImage?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-export const Button: FC<PropsButton> = ({ children, className, type, onClick }) => {
+export const Button: FC<PropsButton> = ({ children, className, type, bgImage, onClick }) => {
   return (
-    <button type={type} onClick={onClick} className={className ? `btn ${className}` : "btn"}>
+    <button type={type} onClick={onClick} style={{backgroundImage: `url(\'${bgImage}\')`}} className={className ? `btn ${className}` : "btn"}>
       {children}
     </button>
   );
