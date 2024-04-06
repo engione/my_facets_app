e7cf704ce7b378d98f368dfa24cc780b9fe537d0
pashastaf <pashastaf1@gmail.com>
2024-04-06 14:21:56 +0300

    need change font-family, size, font-size, make the structure convenient for receiving data from the API

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Test.scss';
 
-interface ButtonProps {
+interface TestButtonProps {
     text: string;
     isActive: boolean;
     onClick: () => void;
 }
 
-export const TestButton: React.FC<ButtonProps> = ({ text, isActive, onClick }) => {
+export const TestButton: React.FC<TestButtonProps> = ({ text, isActive, onClick }) => {
     return (
         <button 
             className={`button-ok ${isActive ? 'active' : ''}`} 
--- src/components/Test/TestButton.tsx
+++ src/components/Test/TestButton.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { TestButton } from "./Test";
 import './TestButton.scss';
 
-let questone:string = 'Совершеннолетние и тренерский состав'
-let questtwo:string = 'Несовершеннолетние и тренерский состав'
-let questthree:string = 'И совершеннолетние, и несовершеннолетние, и тренерский состав'
+//Переменные временное решение, текст должен быть взят из БД.
+const questone:string = 'Совершеннолетние и тренерский состав'
+const questtwo:string = 'Несовершеннолетние и тренерский состав'
+const questthree:string = 'И совершеннолетние, и несовершеннолетние, и тренерский состав'
 
 const types = [questone, questtwo, questthree];
 
