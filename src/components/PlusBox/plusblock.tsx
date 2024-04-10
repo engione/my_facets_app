ac9ebefd5ef63285808b64fb1c4ba15955a8423e
rinebol <stepan13052004@mail.ru>
2024-04-10 10:16:30 +0300

    plusbox

---
+++ src/components/PlusBox/plusblock.tsx
@@ -0,0 +1,25 @@
+
+import "./plusblock.scss"
+
+
+export function plusblock() {
+	return (
+		<div className='MainContainer'>
+			<div className='Block_1 color_1'>
+				<div className='text'>
+					<span>5+ </span>Лет на рынке
+				</div>
+			</div>
+			<div className='Block_1 center color_2'>
+				<div className='text'>
+					<span>1000+ </span>Довольных пользователей
+				</div>
+			</div>
+			<div className='Block_1 color_3'>
+				<div className='text'>
+					<span>5+ </span> Инновационные технологии
+				</div>
+			</div>
+		</div>
+	)
+}
