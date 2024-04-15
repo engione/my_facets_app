7b0321425d9537d6024fee19b9c61ba6ec7f367d
rinebol <stepan13052004@mail.ru>
2024-04-15 13:13:30 +0300

    card_test

--- src/components/people_card/people_card.tsx
+++ src/components/people_card/people_card.tsx
@@ -4,12 +4,17 @@ import './people_card.scss'
 export function people_card() {
 	return (
 		<div className ="card">
-        <img className ="img" src="4082cdf016615143921bfaa5964f79da.png" alt="">
+			  <img 
+                src="4082cdf016615143921bfaa5964f79da.png" 
+                className={`img`} 
+            />
+
         <div className ="info">
             <p className ="name">Васильев Пётр Семёнович</p>
             <p className ="work">Медицинский работник</p>
             <p className ="cost">2500/день</p>
         </div>
+		<button	className='btn'></button>
     </div>
 	)
 }
